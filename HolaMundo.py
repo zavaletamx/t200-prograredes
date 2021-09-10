6e0fb46dbbdfc8e6eab3850ed71da981c286af8a
Raúl Zavaleta <raul.zavaletazea@gmail.com>
2021-09-10 07:50:44 -0500

    Clase 1 Hola Mundo

---
+++ HolaMundo.py
@@ -0,0 +1,14 @@
+# Esta es la clase dos, nuestro primer código
+
+'''
+Este es un comentario mucho mas amplio
+vamos a escribirlo en diversas
+lieneas de código
+'''
+
+"""
+[COPIA] Este es un comentario mucho mas amplio
+vamos a escribirlo en diversas
+lieneas de código
+"""
+print("Hola mundo")
