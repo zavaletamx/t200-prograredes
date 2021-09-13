510f3dd70b8dbf5a360f3ad44cb4b95b3514c726
Raúl Zavaleta <raul.zavaletazea@gmail.com>
2021-09-13 07:49:40 -0500

    Ejemplos de impresión e intro a variables

---
+++ Clase3.py
@@ -0,0 +1,71 @@
+'''
+Para ejecutar un script de Python desde
+terminal / consola / powershwell / símbolos del sistema
+solo necesitamos ubicarnos en la carpeta contenedora de nuestro
+archivo .py
+para ejecutar el comando:
+Para Linux/Mac (Unix): python3 _NOM_ARCHIVO_.py
+Para Windows: python _NOM_ARCHIVO_.py
+'''
+# Las cadenas de texto puedes indicarse con ' o "
+# En python NO UTILIZAMOS ; PARA TERMINAR UNA INSTRUCCIÓN
+# print ("Hola clase 3")
+# print ('Buenos días')
+
+'''
+Una variable es una espacio en RAM reservado para guardar
+un valor de importancia para tu script
+
+Las variables en Python no tiene un prefijo
+
+$linea //PHP
+var linea //JavaScript
+let linea //JavaScript
+const linea //JavaScript
+String linea //Java
+linea //Python
+
+El simbolo de igual indica que se antecede una variable
+donde guardaremos un dato / valor
+
+Los nombres de variable de python
+NO PUEDEN:
+iniciar con numero, no pueden contener espacio
+y no pueden indicarse como palabras reservadas
+no usar caracteres especiales
+**NO USAR ACENTOS NI CARACTERES DEL ESPAÑOL**
+
+Los nombres de variable de python
+PUEDEN:
+iniciar simpre con una letra
+iniciarse o contener en cualquier lugar _
+mayusculas y minusculas
+pueden tener numeros
+
+- camel case
+nombreCompleto
+
+- underscore case
+nombre_completo
+
+Las buenas prácticas recomiendan
+usar siempre camel case
+'''
+linea1 = 'Buenos dias'
+linea2 = "Clase 3"
+linea3 = 'Ya nos vamos'
+
+print(linea1, linea2, 'Nos vemos mañana', linea3)
+
+
+nombre = "Raul"
+
+print('Hola, mi nombres es ', nombre)
+
+
+
+
+
+
+
+
