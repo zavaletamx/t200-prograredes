4135645c69aa2d89f0459ea207e8a677bf4b0e80
Raúl Zavaleta <raul.zavaletazea@gmail.com>
2021-10-18 08:13:01 -0500

    Revisión del proceso de evaluación de la primera unidad

---
+++ Clase9.py
@@ -0,0 +1,71 @@
+'''
+Práctica 3
+
+Número a multiplicar: 8 --> input
+Inicio: 5 --> input
+Fin: 12 --> input
+
+8 x 5 = 40
+8 x 6 = 48
+8 x 7 = 56
+8 x 8 = 64
+8 x 9 = 72
+8 x 10 = 80
+8 x 11 = 88
+8 x 12 = 96
+
+Número a multiplicar: 3 --> input
+Inicio: 9 --> input
+Fin: 3 --> input
+
+3 X 9 = 27
+3 X 8 = 24
+3 X 7 = 21
+3 X 6 = 18
+3 X 5 = 15
+3 X 4 = 12
+3 X 3 = 9
+
+
+Práctica 4
+
+Número de trabajadores: 5 --> input
+
+Sueldo del trabajador {n} : --> input
+Trabajador {n} gana {5000} pesos 
+Su deducción de impuestos: {500} pesos
+Sueldo neto: {4500} pesos
+
+Sueldo del trabajador {n} : --> input
+Trabajador {n} gana {8500} pesos 
+Su deducción de impuestos: {1105} pesos
+Sueldo neto: {7395} pesos
+
+Sueldo del trabajador {n} : --> input
+Trabajador {n} gana {35000} pesos 
+Su deducción de impuestos: {10500} pesos
+Sueldo neto: {24500} pesos
+
+Práctica 5
+Menú del compendio de las prácticas 1, 2, 3 y 4
+
+Generar un menú que permite acceder INFINITAMENTE a las prácticas
+1, 2, 3, 4 y un menú para salir
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
